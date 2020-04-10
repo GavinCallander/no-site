2ee589f9e527d5f4c8697762e9b0f039f6e97683
Gavin Callander <gavincallander@Gavins-MacBook-Pro.local>
2020-04-10 09:20:38 -0400

    holding page for site

--- src/App.js
+++ src/App.js
@@ -5,20 +5,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Thanks for visiting but Sewstrong.org aren't here yet!</h1>
+      <h2>In the meantime, if you're interested in helping out</h2>
+      <h2>or are just looking for some additional information about the project</h2>
+      <h2>why don't you send us an email at</h2>
+      <a href='mailto:info@sewstrong.org'><h2>info@sewstrong.org</h2></a>
+      <h2>or come back tomorrow. The full site will be live within 24 hours</h2>
+      <h2>We greatly appreciate your interest in our cause.</h2>
     </div>
   );
 }
